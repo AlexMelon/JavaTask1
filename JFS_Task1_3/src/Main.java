5ee9ba47af149912cb0eba630a9d7be38637e4e5
AlexMelon <axrajan@gmail.com>
2024-05-06 16:17:10 +0530

    06.05.24

---
+++ JFS_Task1_3/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num = 876;
+        int rev = 0;
+        int rem;
+
+        while (num > 0) {
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+
+            System.out.println(rev);
+        }
+    }
+}
